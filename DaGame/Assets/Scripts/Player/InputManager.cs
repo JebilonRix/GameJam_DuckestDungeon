66668e6b7b06878fec4949bc1b4797c701915c0e
Alpago Göktenay Göçer <alpagogoktenay@hotmail.com>
2021-09-25 14:05:26 +0300

    Input manager updated

--- DaGame/Assets/Scripts/Player/InputManager.cs
+++ DaGame/Assets/Scripts/Player/InputManager.cs
@@ -1,5 +1,3 @@
-using System;
-using SEP;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
@@ -10,14 +8,6 @@ public class InputManager : MonoBehaviour
     KeyCode MoveUp = KeyCode.W, MoveDown = KeyCode.S, MoveRight = KeyCode.D,
         MoveLeft = KeyCode.A, Interaction = KeyCode.E, Confimation = KeyCode.Space;
 
-    private InputFlag _moveUpFlag = new InputFlag(KeyCode.W);
-    private InputFlag _moveDownFlag = new InputFlag(KeyCode.S);
-    private InputFlag _moveRightFlag = new InputFlag(KeyCode.D);
-    private InputFlag _moveLeftFlag = new InputFlag(KeyCode.A);
-    private InputFlag _interactionFlag = new InputFlag(KeyCode.E);
-    private InputFlag _confirmationFlag = new InputFlag(KeyCode.Space);
-    
-    
     private void Awake()
     {
         if (instance == null)
@@ -27,96 +17,43 @@ public class InputManager : MonoBehaviour
     }
     private void Update()
     {
-        
-        _moveUpFlag.SetFlags();
-        _moveDownFlag.SetFlags();
-        _moveRightFlag.SetFlags();
-        _moveLeftFlag.SetFlags();
-        _interactionFlag.SetFlags();
-        _confirmationFlag.SetFlags();
-        
-    }
-
-
-    public void FixedUpdate()
-    {
         #region Movement
-<<<<<<< HEAD
-       
-        if (_moveUpFlag.End)
-        {
-            TopDownMovement.instance.Move(0, 0, false);
-        }
-        else if (_moveUpFlag.Update)
-        {
-            TopDownMovement.instance.Move(0, 1);
-        }
-
-        if (_moveDownFlag.End)
-        {
-            TopDownMovement.instance.Move(0, 0, false);
-=======
         Vector2 movement = Vector2.zero;
         
         if (Input.GetKey(MoveUp))
         {
             movement += Vector2.up;
->>>>>>> alpago
-        }
-        else if (_moveDownFlag.Update)
-        {
-<<<<<<< HEAD
-            TopDownMovement.instance.Move(0, -1);
         }
 
-        if (_moveRightFlag.End)
+        if (Input.GetKey(MoveDown))
         {
-            TopDownMovement.instance.Move(0, 0, false);
-        }
-        else if (_moveRightFlag.Update)
-=======
             movement += Vector2.down;
         }
 
 
         if (Input.GetKey(MoveRight))
->>>>>>> alpago
         {
             movement += Vector2.right;
         }
 
-<<<<<<< HEAD
-        if (_moveLeftFlag.End)
-=======
 
         if (Input.GetKey(MoveLeft))
->>>>>>> alpago
         {
             movement += Vector2.left;
         }
-<<<<<<< HEAD
-        else if (_moveLeftFlag.Update)
-=======
 
 
         if (movement.magnitude < 0.1f)
->>>>>>> alpago
         {
             TopDownMovement.instance.Move(0,0,false);
         }
-<<<<<<< HEAD
-=======
         else
         {
             TopDownMovement.instance.Move(movement.normalized.x, movement.normalized.y,true);
         }
         
         #endregion
->>>>>>> alpago
 
-        #endregion
-        
-        
         if (Input.GetKeyDown(Interaction))
         {
 
@@ -125,13 +62,5 @@ public class InputManager : MonoBehaviour
         {
 
         }
-        
-        _moveUpFlag.ResetStartEndFlags();
-        _moveDownFlag.ResetStartEndFlags();
-        _moveRightFlag.ResetStartEndFlags();
-        _moveLeftFlag.ResetStartEndFlags();
-        _interactionFlag.ResetStartEndFlags();
-        _confirmationFlag.ResetStartEndFlags();
-        
     }
 }
